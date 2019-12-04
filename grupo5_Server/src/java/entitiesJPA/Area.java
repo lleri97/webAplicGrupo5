d2fbdcdbf5db2651b7670f7173ea79cd94d23cdf
Andoni <andonifiat22@gmail.com>
2019-12-04 13:07:54 +0100

    primer commit

--- grupo5_Server/src/java/entitiesJPA/Area.java
+++ grupo5_Server/src/java/entitiesJPA/Area.java
@@ -5,10 +5,102 @@
  */
 package entitiesJPA;
 
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
- * @author Yeray
+ * @author Andoni
  */
-public class Area {
+@Entity
+@Table(name="area",schema="grupo5_database")
+public class Area implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Collection<Department> departments;
+    private Collection<Document> documents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
     
+     /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the departments
+     */
+    public Collection<Department> getDepartments() {
+        return departments;
+    }
+
+    /**
+     * @param departments the departments to set
+     */
+    public void setDepartments(Collection<Department> departments) {
+        this.departments = departments;
+    }
+
+    /**
+     * @return the documents
+     */
+    public Collection<Document> getDocuments() {
+        return documents;
+    }
+
+    /**
+     * @param documents the documents to set
+     */
+    public void setDocuments(Collection<Document> documents) {
+        this.documents = documents;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Area)) {
+            return false;
+        }
+        Area other = (Area) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entitiesJPA.Area[ id=" + id + " ]";
+    }   
 }
--- grupo5_Server/src/java/entitiesJPA/Document.java
+++ grupo5_Server/src/java/entitiesJPA/Document.java
@@ -5,10 +5,57 @@
  */
 package entitiesJPA;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
- * @author Yeray
+ * @author Andoni
  */
-public class Document {
+@Entity
+@Table(name="document",schema="grupo5_database")
+public class Document implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Document)) {
+            return false;
+        }
+        Document other = (Document) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entitiesJPA.Document[ id=" + id + " ]";
+    }
     
 }
